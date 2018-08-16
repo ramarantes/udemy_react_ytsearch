721c71fe543de9adc2ad3a3e4b20c6415e08a699
Rafael Amarantes <rafael@amarantes.com.br>
2018-08-15 23:59:18 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyAl0wswOThHjJO0TyoK_hyq9C5ih1uTlVE';
 
-import VideoList from './components/video_list'
+import VideoList from './components/video_list'  
 
 class App extends Component{
     constructor(props) {
